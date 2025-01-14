42ce7c2f0a6391718ee822ac4d4b0ffb3efcc454
cj <3011976305@qq.com>
2025-01-14 17:41:51 +0800

    2.微服务之间的请求（携带当前用户的信息），openFeign拦截器

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,11 +1,26 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level defaultFeignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor UserInfoInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                if (UserContext.getUser() != null) {
+                    requestTemplate.header("user-info", UserContext.getUser().toString());
+                }
+            }
+        };
     }
 }
